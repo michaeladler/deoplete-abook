b028071c1adcae707dfb18f03226592325732449
Filip Szymański <filip.szymanski@zoho.eu>
2017-06-16 14:24:51 +0200

    Initial release

---
+++ rplugin/python3/deoplete/source/abook.py
@@ -0,0 +1,54 @@
+# Copyright (c) 2017 Filip Szymański. All rights reserved.
+# Use of this source code is governed by an MIT license that can be
+# found in the LICENSE file.
+
+import configparser
+import re
+
+from .base import Base  # pylint: disable=E0401
+
+class Source(Base):
+    COLON_PATTERN = re.compile(r':\s?')
+    COMMA_PATTERN = re.compile(r'.+,\s?')
+    HEADER_PATTERN = re.compile(r'^(Bcc|Cc|From|Reply-To|To):(\s?|.+,\s?)')
+
+    def __init__(self, vim):
+        super().__init__(vim)
+
+        self.__cache = []
+
+        self.filetypes = ['mail']
+        self.mark = '[ABOOK]'
+        self.matchers = ['matcher_length', 'matcher_full_fuzzy']
+        self.min_pattern_length = 0
+        self.name = 'abook'
+
+    def __make_cache(self):
+        addressbook = configparser.ConfigParser()
+        addressbook.read(self.vim.eval('g:deoplete_abook_datafile'))
+        for section in addressbook.sections():
+            emails = addressbook.get(section, 'email', fallback=None)
+            if emails is not None:
+                name = addressbook.get(section, 'name', fallback=None)
+                for email in emails.split(','):
+                    if name is not None:
+                        email = '{0} <{1}>'.format(name, email)
+
+                    self.__cache.append({'word': email})
+
+    def gather_candidates(self, context):
+        if self.HEADER_PATTERN.search(context['input']) is not None:
+            if not self.__cache:
+                self.__make_cache()
+
+            return self.__cache
+
+    def get_complete_position(self, context):  # pylint: disable=R0201
+        colon = self.COLON_PATTERN.search(context['input'])
+        comma = self.COMMA_PATTERN.search(context['input'])
+        return max(colon.end() if colon else -1, comma.end() if comma else -1)
+
+    def on_event(self, context):  # pylint: disable=W0613
+        self.__make_cache()
+
+# vim: ts=4 et sw=4
