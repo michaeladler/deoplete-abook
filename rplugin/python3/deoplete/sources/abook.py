6e17e1da18861f63f1cd1f73f54259b2e8ca9b78
Filip Szymański <filip.szymanski@zoho.eu>
2017-06-18 11:22:38 +0200

    Check if addressbook file exists

--- rplugin/python3/deoplete/sources/abook.py
+++ rplugin/python3/deoplete/sources/abook.py
@@ -8,6 +8,7 @@ import re
 
 from .base import Base  # pylint: disable=E0401
 
+# pylint: disable=W0201,W0613
 class Source(Base):
     COLON_PATTERN = re.compile(r':\s?')
     COMMA_PATTERN = re.compile(r'.+,\s?')
@@ -24,12 +25,31 @@ class Source(Base):
         self.min_pattern_length = 0
         self.name = 'abook'
 
-    def __make_cache(self, context):
-        datafile = context['vars'].get('deoplete#sources#abook#datafile',
-                                       os.path.expanduser('~/.abook/addressbook'))
+    def on_init(self, context):
+        self.__datafile = context['vars'].get('deoplete#sources#abook#datafile',
+                                              os.path.expanduser('~/.abook/addressbook'))
+        if not os.path.isfile(self.__datafile):
+            self.vim.err_write('[deoplete-abook] No such file: {0}\n'.format(self.__datafile))
 
+    def on_event(self, context):
+        self.__make_cache()
+
+    def gather_candidates(self, context):
+        if self.HEADER_PATTERN.search(context['input']) is not None:
+            if not self.__cache:
+                self.__make_cache()
+
+            return self.__cache
+
+    def get_complete_position(self, context):
+        colon = self.COLON_PATTERN.search(context['input'])
+        comma = self.COMMA_PATTERN.search(context['input'])
+        return max(colon.end() if colon is not None else -1,
+                   comma.end() if comma is not None else -1)
+
+    def __make_cache(self):
         addressbook = configparser.ConfigParser()
-        addressbook.read(datafile)
+        addressbook.read(self.__datafile)
         for section in addressbook.sections():
             emails = addressbook.get(section, 'email', fallback=None)
             if emails is not None:
@@ -39,21 +59,5 @@ class Source(Base):
                         email = '{0} <{1}>'.format(name, email)
 
                     self.__cache.append({'word': email})
-
-    def gather_candidates(self, context):
-        if self.HEADER_PATTERN.search(context['input']) is not None:
-            if not self.__cache:
-                self.__make_cache(context)
-
-            return self.__cache
-
-    def get_complete_position(self, context):  # pylint: disable=R0201
-        colon = self.COLON_PATTERN.search(context['input'])
-        comma = self.COMMA_PATTERN.search(context['input'])
-        return max(colon.end() if colon is not None else -1,
-                   comma.end() if comma is not None else -1)
-
-    def on_event(self, context):
-        self.__make_cache(context)
 
 # vim: ts=4 et sw=4
